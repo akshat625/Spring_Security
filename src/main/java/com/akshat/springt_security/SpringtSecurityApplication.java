6594db6623107843bfd4a4133e81e57a0e5df729
Akshat Srivastava <akshatsrivastava625@gmail.com>
2024-08-02 16:28:33 +0530

    Spring Security

---
+++ src/main/java/com/akshat/springt_security/SpringtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.akshat.springt_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akshat/springt_security/SpringtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshat.springt_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
